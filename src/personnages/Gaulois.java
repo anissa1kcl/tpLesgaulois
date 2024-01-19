b01f42201d578dbd215896dc8454e365da5e81a0
anissa1kcl <anissa.kecili@icloud.com>
2024-01-19 09:22:57 +0100

    TP1 question4

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,11 @@
+package personnages;
+
+public class Gaulois {
+ public String nom; 
+ public int force; 
+ 
+ public Gaulois (String paramNom, int paramForce) {
+	this.force = paramForce;
+	this.nom = paramNom ;
+ }
+}
